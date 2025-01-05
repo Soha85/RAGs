8a20bbc5f02a8fafa7193771f2599151cf89f72a
Soha <sohaelshafey@yahoo.com>
2025-01-05 10:32:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,72 +12,21 @@ from RetrievalComponent import RetrievalComponent
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 st.set_page_config(layout="wide")
-
-
-
-st.session_state.articles_df = RAG.articles
-# Streamlit UI
 st.title("Different RAGs Approaches")
 
-def evaluate_rouge(answer,reference):
-    if answer:
-        evaluator = rouge.Rouge()
-        return evaluator.get_scores(answer, reference)
-    else:
-        return "no score"
+# Display articles in a table (if any)
+if not st.session_state.articles_df.empty:
+    st.write(st.session_state.articles_df)
+else:
+    st.info("No articles scraped yet.")
+# Clear controls when selected website changes
+if selected_website != st.session_state.previous_website:
+    st.session_state.previous_website = selected_website
+    question = ""
+    st.empty()
+    st.session_state.articles_df = RAG.article
 
-# Function to scrape article URLs from a website
-def scrape_articles(site_url):
-    try:
-        response = requests.get(site_url, timeout=10)  # Add a timeout for safety
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error fetching the article: {e}")
-        return None, f"Error fetching the article: {e}"
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Extract text within the article (based on inspection)
-    div_elements = soup.find_all('div', {'data-component': 'text-block'})
-    all_paragraphs = []
-    title = soup.title.get_text()
-    for div in div_elements:
-        paragraphs = div.find_all('p')
-        for para in paragraphs:
-            all_paragraphs.append(para.get_text())
-
-    full_text = ' '.join(all_paragraphs)
-    return title, full_text.strip()
-
-def rag_generate(query,context):
-    try:
-        articles_llm = pipeline(task='text-generation', model=selected_model)
-        #articles_llm.model.config.pad_token_id = articles_llm.model.config.eos_token_id
-        generated = articles_llm(f"Query: {query}\nContext: {context}\nAnswer:",max_new_tokens=100,temperature=temperature,num_return_sequences=1)
-        # Check if the generation was successful and retrieve the generated text
-        if generated and len(generated) > 0:
-            generated_text = generated[0]['generated_text'].split('Answer')[1]
-            return generated_text
-        else:
-            st.write("No output generated. Please check the model or input parameters.")
-            return None
-        #return generated[0]['generated_text'].split('Answer:')[1]
-
-    except Exception as e:
-        st.write(f"Error generating text: {e}")
-        return None
-
-def call_RAG_generate(query, context):
-    ans = rag_generate(query, context)
-    st.write(f"Generated Answer:{ans}")
-    st.write(f"Evaluation:{evaluate_rouge(ans, context)}")
-
-def call_metrices(query,context):
-    cosine_score = N_RAG.calculate_cosine_similarity(query, context)
-    st.write(f"Best Cosine Similarity score:{cosine_score}")
-    enhanced_score = N_RAG.calculate_enhanced_similarity(query, context)
-    st.write(f"Enhanced Similarity score:{enhanced_score}")
 
 # Session state for storing scraped data
 if "articles_df" not in st.session_state:
@@ -124,12 +73,6 @@ if st.button('Get Articles'):
     except requests.exceptions.RequestException as e:
         st.error(f"Failed to fetch articles: {e}")
 
-# Display articles in a table (if any)
-if not st.session_state.articles_df.empty:
-    st.write(st.session_state.articles_df)
-else:
-    st.info("No articles scraped yet.")
-
 # Input for user question
 question = st.text_input("Ask a question:")
 chunk_size = st.number_input("Chunk Size", min_value=10, max_value=500, value=100, step=50)
@@ -138,8 +81,6 @@ overlap = st.number_input("Overlap Size", min_value=10, max_value=500, value=10,
 temperature = st.slider("Temperature", min_value=0.0, max_value=1.5, value=0.7, step=0.1)
 models_list = ['gpt2','EleutherAI/gpt-neo-2.7B','EleutherAI/gpt-j-6B','EleutherAI/gpt-neox-20b','t5-large','bigscience/bloom-3b','facebook/opt-6.7b','google/flan-t5-large','meta-llama/LLaMA-7b-hf']
 selected_model=st.selectbox("Select a model:",models_list)
-
-
 
 # In each column, you can display different RAG outputs
 # Button to send the question for processing
@@ -207,15 +148,61 @@ if st.button('Ask Question'):
 
 
 
+def evaluate_rouge(answer,reference):
+    if answer:
+        evaluator = rouge.Rouge()
+        return evaluator.get_scores(answer, reference)
+    else:
+        return "no score"
 
+# Function to scrape article URLs from a website
+def scrape_articles(site_url):
+    try:
+        response = requests.get(site_url, timeout=10)  # Add a timeout for safety
+        response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching the article: {e}")
+        return None, f"Error fetching the article: {e}"
 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Extract text within the article (based on inspection)
+    div_elements = soup.find_all('div', {'data-component': 'text-block'})
+    all_paragraphs = []
+    title = soup.title.get_text()
+    for div in div_elements:
+        paragraphs = div.find_all('p')
+        for para in paragraphs:
+            all_paragraphs.append(para.get_text())
 
+    full_text = ' '.join(all_paragraphs)
+    return title, full_text.strip()
 
+def rag_generate(query,context):
+    try:
+        articles_llm = pipeline(task='text-generation', model=selected_model)
+        #articles_llm.model.config.pad_token_id = articles_llm.model.config.eos_token_id
+        generated = articles_llm(f"Query: {query}\nContext: {context}\nAnswer:",max_new_tokens=100,temperature=temperature,num_return_sequences=1)
+        # Check if the generation was successful and retrieve the generated text
+        if generated and len(generated) > 0:
+            generated_text = generated[0]['generated_text'].split('Answer')[1]
+            return generated_text
+        else:
+            st.write("No output generated. Please check the model or input parameters.")
+            return None
+        #return generated[0]['generated_text'].split('Answer:')[1]
 
+    except Exception as e:
+        st.write(f"Error generating text: {e}")
+        return None
 
-# Clear controls when selected website changes
-if selected_website != st.session_state.previous_website:
-    st.session_state.previous_website = selected_website
-    question = ""
-    st.empty()
-    st.session_state.articles_df = RAG.articles
+def call_RAG_generate(query, context):
+    ans = rag_generate(query, context)
+    st.write(f"Generated Answer:{ans}")
+    st.write(f"Evaluation:{evaluate_rouge(ans, context)}")
+
+def call_metrices(query,context):
+    cosine_score = N_RAG.calculate_cosine_similarity(query, context)
+    st.write(f"Best Cosine Similarity score:{cosine_score}")
+    enhanced_score = N_RAG.calculate_enhanced_similarity(query, context)
+    st.write(f"Enhanced Similarity score:{enhanced_score}")
+
